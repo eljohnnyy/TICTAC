a02725353465978861167d1773916f21d05ae5b6
eljohnnyy <you@example.com>
2023-09-09 06:01:34 +0300

    home view part one

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tictac/game_logic.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -8,10 +9,57 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  String activeplayer='X';
+  bool gameover=false;
+  int turn=0;
+  String result='xxxxxxxx';
+  Game game=Game();
+  bool isswitch=false;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
+      
+      backgroundColor: Theme.of(context).primaryColor,
+body:  SafeArea(child: Column(
+  
+  children: [
+SwitchListTile.adaptive(
+  
+  title:const Text('Turn On/Off Two Player',style: TextStyle(
+    fontSize: 28,
+    
+  ),
+  textAlign: TextAlign.center,
+  ),
+  value: isswitch, onChanged: (newvalue){
+  setState(() {
+    isswitch=newvalue;
+  });
+}),
+ Text('it\'s $activeplayer turn'.toUpperCase(),style:const  TextStyle(
+    fontSize: 52,
+    
+  )),
+   Text(result.toUpperCase(),style:const  TextStyle(
+    fontSize: 42,
+    
+  )),
+  ElevatedButton.icon(
+    style: ButtonStyle(
+      backgroundColor:MaterialStateProperty.all(Theme.of(context).splashColor) ,
+    ),
+    onPressed: (){
+    setState(() {
+       activeplayer='X';
+  gameover=false;
+  turn=0;
+  result='';
+    });
+  }, icon:const Icon(Icons.replay), label:const Text('Repeat the game'),
 
+  )
+  ],
+)),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class TicTac extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+      debugShowCheckedModeBanner: false,
 theme: ThemeData(
 brightness: Brightness.dark,
 primarySwatch: Colors.blue,
